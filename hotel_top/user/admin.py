c01f3e395201d811bfa262245697d01a29d2c239
Dimus73 <dmitry.prigozhin@gmail.com>
2023-05-12 19:45:40 +0300

    Finish with models

--- hotel_top/user/admin.py
+++ hotel_top/user/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from.models import *
 
 # Register your models here.
+admin.site.register(Review)
+admin.site.register(Inquiry)
+admin.site.register(RoomType)
+admin.site.register(AdultsCount)
+admin.site.register(Booking)
+admin.site.register(Rooms)
+admin.site.register(RommPhoto)
+
--- hotel_top/user/models.py
+++ hotel_top/user/models.py
@@ -1,35 +1,47 @@
+from datetime import datetime
 from django.db import models
 
 # Create your models here.
 class Review (models.Model):
-    text=models.TextField()
-    email=models.EmailField(null=True)
+    text           = models.TextField()
+    email          = models.EmailField(null=True, blank=True)
 
 
 class Inquiry(models.Model):
-    text=models.TextField()
-    email=models.EmailField(null=True)
-    answered=models.BooleanField()
+    text           = models.TextField()
+    email          = models.EmailField(null=True)
+    is_answered    = models.BooleanField(default=False)
+
+class RoomType (models.Model):
+    type           = models.CharField(max_length=50)
+    def __str__(self) -> str:
+        return self.type
+
+class AdultsCount (models.Model):
+    count          = models.CharField(max_length=10)
+    def __str__(self) -> str:
+        return self.count
 
 class Rooms(models.Model):
-    people = models.CharField(max_length=100)
-    type = models.CharField(max_length=50)
-    price = models.FloatField(null=False)
-
-class Roomtype(models.Model):
-    type=models.CharField(max_length=25)
-
-class Roompeople(models.Model):
-    quantity=models.IntegerField()
+    adults         = models.ForeignKey(AdultsCount,on_delete=models.DO_NOTHING)
+    type           = models.ForeignKey(RoomType, on_delete=models.DO_NOTHING)
+    price          = models.FloatField(null=False)
+    quantity       = models.IntegerField(default=0)
+    def __str__(self) -> str:
+        return self.type.type + ":" + self.adults.count
 
 class Booking(models.Model):
-    room = models.ForeignKey(Rooms, on_delete=models.CASCADE)
-    date_beginning=models.DateField()
-    date_end=models.DateField()
-    client=models.EmailField()
-    paid=models.BooleanField()
+    room           = models.ForeignKey(Rooms, on_delete=models.DO_NOTHING)
+    date_beginning = models.DateField()
+    date_end       = models.DateField()
+    client         = models.IntegerField(null=True,blank=True)
+    paid           = models.BooleanField()
+    def __str__(self) -> str:
+        defferense = self.date_end - self.date_beginning
+        return f"Booking from {self.date_beginning} for {defferense.days} days"
 
-class Client(models.Model):
-    name = models.CharField(max_length=50)
-    email = models.EmailField()
-    reviews = models.ForeignKey(Review, null=True, on_delete=models.CASCADE)
+class RommPhoto(models.Model):
+    room           = models.ForeignKey(Rooms,on_delete=models.DO_NOTHING)
+    photo          = models.ImageField(upload_to="photos/%Y/%m/%d/")
+    def __str__(self) -> str:
+        return f"Photo for {self.room}"
