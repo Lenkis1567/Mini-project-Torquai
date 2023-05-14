e10eb7ec744178813e449701c4aba99712ea40c4
Lenkis1567 <chuvilinaelena.manager@gmail.com>
2023-05-14 13:37:15 +0300

    changed htmls, urtl and added booking delete

--- hotel_top/hotel/urls.py
+++ hotel_top/hotel/urls.py
@@ -22,7 +22,8 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('user.urls')),
-    path('', include('staff.urls'))
+    path('staff', include('staff.urls'))
+
     
 ]
 
