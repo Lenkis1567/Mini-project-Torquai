9ca34df7f741815e8f76e1b84c33a9c8b1300bf7
Lenkis1567 <chuvilinaelena.manager@gmail.com>
2023-05-15 13:36:05 +0300

    1505-1

--- hotel_top/staff/views.py
+++ hotel_top/staff/views.py
@@ -65,6 +65,11 @@ class ReviewsStaffListView(DataMixin, ListView):
     model = Review
     template_name = 'staff/reviews.html' 
     context_object_name = 'reviews'
+    def get_context_data(self, *,object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        c_def = self.get_user_context(title = "")
+        context = dict(list(context.items()) + list(c_def.items()))
+        return context
     
 
 class ReviewDeleteView(DataMixin, DeleteView):
