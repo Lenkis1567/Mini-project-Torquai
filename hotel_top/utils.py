2809a9caae1707417c999ba132758203b221b325
Lenkis1567 <chuvilinaelena.manager@gmail.com>
2023-05-15 10:27:18 +0300

    small changes in menu

--- hotel_top/utils.py
+++ hotel_top/utils.py
@@ -1,7 +1,7 @@
 menu = [{'title': "Home", 'url_name': 'main_page_path', 'for_s_user':False},
         {'title': "Search the room", 'url_name': 'search_path', 'for_s_user':False},
-    #    {'title': "Reviews", 'url_name': 'reviews', 'for_s_user':True},
-#        {'title': "inquiries", 'url_name': 'inquiries', 'for_s_user':False}
+       {'title': "Reviews", 'url_name': 'client_reviews', 'for_s_user':True},
+       {'title': "Inquiries", 'url_name': 'inquiries', 'for_s_user':False}
         ]
 menu_staff = [{'title': "Home", 'url_name': 'main_page_staff', 'for_s_user':True},
         {'title': "Bookings", 'url_name': 'booking_staff', 'for_s_user':True},
